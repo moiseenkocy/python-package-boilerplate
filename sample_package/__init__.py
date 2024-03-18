8aa816016d9fb2a553e96305ed9a0a0a5670c3b9
Andrei Moiseenko <moiseenko.cy@gmail.com>
2024-03-18 23:06:06 +0200

    Add hello_world implementation.

---
+++ sample_package/__init__.py
@@ -0,0 +1,19 @@
+"""Sample package."""
+
+def hello_world() -> str:
+    """Return "Hello, World!".
+
+    Args:
+    ----
+        None
+
+    Returns:
+    -------
+        A string containing 'Hello, World!'
+
+    Raises:
+    ------
+        None
+
+    """
+    return "Hello, World!"
